decf06fecaab947fc2c38ce89fce9e8379bf2d39
Liam Sykes <liamasykes@gmail.com>
2023-05-01 17:59:45 -0400

    Added coded for some constructors

--- blackjack.py
+++ blackjack.py
@@ -1,6 +1,14 @@
 class Blackjack:
     """Blackjack Class"""
-    
+
+    def __init__(self):
+        self.createUsers()
+
+    def createUsers(self):
+        computerUser = Computer()
+        playerUser = Player()
+        cards = Cards()
+
 
 class Users:
     """Users Class"""
@@ -9,22 +17,40 @@ class Users:
 class Computer(Users):
     """Computer Class"""
 
+    def __init__(self):
+        self.name = "Dealer"
+        print("My name is " + self.name)
+
 
 class Player(Users):
     """Player Class"""
 
+    def __init__(self):
+        self.name = "Player"
+        print("My name is " + self.name)
+
+
 class Deck:
     """Deck Class"""
-
+    
+    def __init__(self):
+        self.suits = { "Clubs" : "♧", "Spades" : "♤", "Hearts" : "♡", "Diamonds" : "♢" }
+        self.cards = Cards()
 
 class Cards(Deck):
     """Cards Class"""
 
+    def __init__(self):
+        self.total = 52
+    
 
 if __name__ == '__main__':
     print("Starting blackjack.py...\n\n")
     userInput = input("\"/start\" to play!\n")
     while(userInput != "/start"):
-        userInput = input("Invalid command...\"/help\" to see list of commands.\n")
-        if userInput == "/help": print("\nHelp Menu\n")
+        if userInput == "/help":
+            print("\nHelp Menu\n")
+            userInput = input()
+        else: userInput = input("Invalid command...\"/help\" to see list of commands.\n")
+        
     Blackjack()
